1950283fa0e9a8ed4b74618c2bed63f807a87381
Carlj0666 <carlj1881@yahoo.com>
2021-04-13 13:51:03 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Add your functions here
+
+function map(array, callBackFunction) {
+  let newArray = []
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callBackFunction(array[i]))
+  }
+  return newArray
+}
+
+function reduce(array, callBackFunction, ) {
+
+}
+
