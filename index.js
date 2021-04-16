dcff7ab6c98027156b9c78c3ae7f296669e7ad0b
Carlj0666 <carlj1881@yahoo.com>
2021-04-16 12:34:59 -0700

    commit tree 36ec657afa8331dd80037ad4fa05693b757785a2

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ function map(array, callBackFunction) {
   return newArray
 }
 
-function reduce(array, callBackFunction, ) {
-
+function reduce(array, callBackFunction) {
+  console.log(callBackFunction())
+  let total = 0
+  for (let i = 0; i < array.length; i++) {
+    total += array[i]
+  }
+  return total
 }
 
